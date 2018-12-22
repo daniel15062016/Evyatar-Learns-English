d4c7f8b59c8c74bee9f428b1e86075b18cbcee31
daniel15062016 <daniel15062016@gmail.com>
2018-12-22 11:38:16 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -21,19 +21,3 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/logo-white.png');
     }
   }
-
-/*
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/logo-white.png') {
-      myImage.setAttribute ('src','images/logo-green.jpg');
-    }
-    else if (mySrc === 'images/logo-green.png') {
-      myImage.setAttribute ('src','images/logo-grey.jpg');
-    }
-    else {
-    myImage.setAttribute ('src','images/logo-white.png');
-    }
-*/
